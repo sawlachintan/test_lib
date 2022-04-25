67a6c7f09ca9ad4b987cf226d9091275761539bc
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2022-04-25 15:32:21 -0400

    clean slate V2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="msds",
-    version="0.0.2",
+    version="0.0.3",
     author="Soutick Saha, Chintan Sawla",
     author_email="soutick2010@gmail.com, sawlachintan@gmail.com",
     description="MSDS Package",
@@ -15,20 +15,19 @@ setuptools.setup(
     project_urls={
         "Bug Tracker": "https://github.com/sawlachintan/msds-library/issues",
     },
+    packages=find_packages(exclude="tests"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_data={
-        'msds':['*.xlsx']
+        'msds': ['*.xlsx']
     },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     install_requires=[
-       "pandas==1.2.4",
-       "pdfplumber",
-       "openpyxl",
-   ],
+        "pandas==1.2.4",
+        "pdfplumber",
+        "openpyxl",
+    ],
 )
