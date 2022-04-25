37d9b820813f3f2b49f16b7cb5b2b5d7c52ff3ec
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2022-04-25 16:47:00 -0400

    change upload destination V4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="msds",
-    version="0.0.4",
+    version="0.0.5",
     author="Soutick Saha, Chintan Sawla",
     author_email="soutick2010@gmail.com, sawlachintan@gmail.com",
     description="MSDS Package",
